2641de39d5f311f6624df47239f71d95f54ecb74
Adrien BAUDE <Vendicare@localhost.localdomain>
2017-12-12 11:32:27 +0100

    commit

--- Views/tagView.py
+++ Views/tagView.py
@@ -1,6 +1,7 @@
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
+import os
 
 from Views import labelView
 
@@ -13,18 +14,18 @@ class Tag(QWidget):
         self.setLayout(layout)
         box = labelView.Label()
         cross = labelView.Label()
-        cross.setPixmap(QPixmap('/home/Vendicare/Projects/PyCharm/Photophore/assets/cross.png').scaled(25, 25, Qt.KeepAspectRatio))
+        cross.setPixmap(QPixmap(os.getcwd()+'/assets/cross.png').scaled(25, 25, Qt.KeepAspectRatio))
         text = QLabel(text)
         layout.addWidget(box)
         layout.addWidget(text)
         layout.addWidget(cross)
         if not check:
             self.layout().itemAt(0).widget().setPixmap(
-                QPixmap('/home/Vendicare/Projects/PyCharm/Photophore/assets/unchecked.png').scaled(25, 25,
+                QPixmap(os.getcwd()+'/assets/unchecked.png').scaled(25, 25,
                                                                                                    Qt.KeepAspectRatio))
         else:
             self.layout().itemAt(0).widget().setPixmap(
-                QPixmap('/home/Vendicare/Projects/PyCharm/Photophore/assets/checked.png').scaled(25, 25,                                                                                            Qt.KeepAspectRatio))
+                QPixmap(os.getcwd()+'/assets/checked.png').scaled(25, 25,                                                                                            Qt.KeepAspectRatio))
 
     def mouseReleaseEvent(self, event):
         self.clicked.emit()
